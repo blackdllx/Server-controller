086a67956e0583ef62b8e3b7a6e94180cd93354d
blackdllx <danylo.wowk@gmail.com>
2023-04-19 18:57:32 +0300

    save job

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import mctools
+import mcrcon
 import requests
 import subprocess
-import random2
+import random
 import re
 import os
+import string
 import threading
 import time
 
@@ -15,7 +16,7 @@ class Server:
     def __init__(self, name: str, _id=None, core: str = "vanilla", version: str = "1.19.2", pirate: bool = False) -> None:
         self.name = name
         self.id = _id
-        self.config = {
+        self.config = { # TODO: Config saver to json
             "memory": "1024M"
         }
         self.log=b""
@@ -24,12 +25,13 @@ class Server:
         self.running=False
         self.pipe=None
         self.properties={}
-        self.password=''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N))
+        self.password=''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(10))
 
         self.CreateServer()
         self.DownloadServer(core, version)
         self.InnitServer()
-        self.PropertiesParce()
+        
+        self.WhenRunning(self.CreatePipe)
 
 
     def CreateServer(self) -> None:
@@ -51,6 +53,9 @@ class Server:
             os.makedirs(f"Servers1/{self.id}")
     
     def DownloadServer(self, core, version) -> None:
+        # TODO: Core change
+
+        
         if os.path.isfile(f"Servers1/{self.id}/Server.jar"):return
         match core:
             case "vanilla":
@@ -73,6 +78,7 @@ class Server:
         self.process = subprocess.Popen(f"java -jar -Xmx{self.config['memory']} -Xms{self.config['memory']} Server.jar -nogui", shell=True, 
                                         cwd=f"./Servers1/{self.id}/", stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         self.active = True
+        self.PropertiesParce()
         print("Start logging")
         threading.Thread(target=self.EventsHandler).start()
         threading.Thread(target=self.Logging).start()
@@ -95,28 +101,39 @@ class Server:
                     if not i in self.errors:
                         print("Error found")
                         self.errors.append(i)
-                        print(self.errors)
+                        
 
         while self.active:
             time.sleep(0.2)
             if re.search(r"\[Server thread\/INFO\]: Done", self.log.decode()) and not self.running:
                 print('Done found')
                 self.running = True
+            
+            if re.search(r"Stopping the server", self.log.decode()):
+                print("Server stopped")
+                self.running=False
+                self.active=False
+                self.log=b""
 
             ErrorHandler(self)
     
     def PropertiesParce(self):
+        print("Parcing properties")
         with open(f"Servers1/{self.id}/server.properties", "rb") as f:
             for i in f.read().decode().split("\n"):
-                if i[0] != "#":
-                    self.properties.update({i.split("=")[0]:i.split("=")[1]})
+                print(i)
+                if not i == "":
+                    if i[0] != "#":
+                        self.properties.update({i.split("=")[0]:i.split("=")[1]})
 
     def PropertiesSave(self):
-        with open(f"Servers1/{self.id}/server.properties", "wb") as f:
+        print("Save properties")
+        with open(f"Servers1/{self.id}/server.properties", "w") as f:
             for i in self.properties:
-                f.write(f"{i}={self.properties[i]}")
+                f.write(f"{i}={self.properties[i]}\n")
 
     def CreatePipe(self):
+        print("creating pipe")
         flag=False
         if self.properties["enable-rcon"] == "false":
             self.properties["enable-rcon"]="true"
@@ -127,25 +144,37 @@ class Server:
         else:
             self.password=self.properties["rcon.password"]
         
-        if flag:
-            self.process.stdin.write(b"stop") # Add stop event to event Handler
-            self.process.stdin.flush()
-            self.active=False
-            self.running=False
-            self.PropertiesSave()
-            self.InnitServer()
-            while not self.running:
-                time.sleep(0.1)
-            
-
-        self.pipe = mctools.RCONClient(f"localhost", self.properties["server-port"])
-        self.pipe.login(self.password)
         
-        if not self.pipe.is_authenticated():
-            raise LookupError
-
-
+        if flag:
+            self.process.stdin.write(b"stop\n")
+            self.process.stdin.flush()
+            while self.running:
+                time.sleep(0.1)
+            self.PropertiesSave()
+            os.remove(f"Servers1/{self.id}/world/session.lock")
+            self.InnitServer()
             
+        self.WhenRunning(print, 1)
+        time.sleep(1)
+        self.pipe = mcrcon.MCRcon("localhost", self.password, self.properties["rcon.port"])
+        
+        print("Pipe created")
+        
+
+    def WhenRunning(self, func, *args):
+        while not self.running:
+            time.sleep(0.1)
+        print(args)
+        if not args == ():
+            
+            func(args)
+            return
+        func()
+        
+    def Stop(self):
+        pass
+        
+
 
 
 class Cord:
